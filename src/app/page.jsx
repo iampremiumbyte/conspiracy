384163e5e516a986dbaeddb234fe740ba77da70d
Godwin Idemudia <godwinide@gmail.com>
2024-04-04 23:48:30 +0100

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const home = () => {
+  
+  return (
+    <div className='flex items-center justify-center w-screen h-screen'>
+      <img src='/PAGE 1 - TEMPLATES/BACKGROUND TEXTURE 1920X1080.png' className='w-full h-full absolute' />
+      <div className="absolute w-full h-full flex items-center justify-center w-screen h-screen">
+        <button className='w-1/4 cursor-pointer'>
+          <img src='/PAGE 1 - TEMPLATES/TRIANGLE.png' className='w-full' />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default home
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Intro = ({ref}) => {
+  return (
+    <div>
+        <video src='/intro.mp4'/>
+    </div>
+  )
+}
+
+export default Intro
