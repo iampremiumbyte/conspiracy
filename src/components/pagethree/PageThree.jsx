e307505a58289390ff61ae007fef0575033d0b07
Godwin Idemudia <godwinide@gmail.com>
2024-04-05 01:47:37 +0100

    new

--- src/components/pagethree/PageThree.jsx
+++ src/components/pagethree/PageThree.jsx
@@ -7,22 +7,14 @@ const PageThree = ({level}) => {
     const [code, setCode] = useState("666-");
 
     const handleSubmit = () => {
-        if(code.length !== 7){
-            return false
-        }
-
         if(!code.includes("666-")){
             alert("Code must begin with 666-");
             return;
         }
 
-        const randomStr = generateRandomString();
-
-        const code = `666-${randomStr}`
-
-        const tweet = `I am now solving the biggest  %23COINSPIRACY %0A%0A Enter my code to join the mystery: ${_code} %0A%0A @CoinspiracySOL`;
+        const tweet = `I am now solving the biggest  %23COINSPIRACY %0A%0A Enter my code to join the mystery: ${ `666-${generateRandomString().toLocaleUpperCase()}`} %0A%0A @CoinspiracySOL`;
         const url = `https://twitter.com/intent/tweet?text=${tweet}`
-        window.open(url);
+        window.location.href = url;
 
     }
   return (
@@ -30,12 +22,12 @@ const PageThree = ({level}) => {
       <img src='/PAGE 2 - TEMPLATES/BG-TEXTURE 1920X1080.png' className='w-full h-full absolute' />
       <div className="flex flex-col justify-center items-center gap-6  absolute w-full h-full">
           <img src='/PAGE 2 - TEMPLATES/TRIANGLE.png' className='w-1/2 sm:w-1/5' />
-          <div className="w-1/2 sm:w-1/4 relative">
+          <div className="w-full sm:w-1/4 relative">
               <img src='/PAGE 2 - TEMPLATES/CODESPACE.png' className='w-full cursor-pointer' />
               <input 
                 type="text" 
                 value={code} 
-                maxLength={7}
+                maxLength={12}
                 onInput={e => setCode(e.target.value)}
                 className='absolute w-full h-full top-0 bg-transparent text-black font-black text-center'
                 style={{
@@ -48,7 +40,7 @@ const PageThree = ({level}) => {
                 }}
             />
           </div>
-          <img src='/PAGE 2 - TEMPLATES/ENTERCODE.png' className='w-1/2 sm:w-1/4 cursor-pointer' onClick={handleSubmit} />
+          <img src='/PAGE 2 - TEMPLATES/ENTERCODE.png' className='w-2/3 sm:w-1/4 cursor-pointer' onClick={handleSubmit} />
       </div>
     </div>
   )
