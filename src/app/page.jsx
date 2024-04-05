e21640b76b926b5d553a016708ce103083b48afa
Godwin Idemudia <godwinide@gmail.com>
2024-04-05 01:02:47 +0100

    new

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,38 +1,9 @@
-"use client"
-
-import Intro from '@/components/intro/Intro';
-import PageOne from '@/components/pageone/PageOne';
-import PageThree from '@/components/pagethree/PageThree';
-import PageTwo from '@/components/pagetwo/PageTwo';
-import React, { useRef, useState } from 'react'
+import Main from '@/components/main/Main'
+import React from 'react'
 
 const home = () => {
-  const [level, setLevel] = useState(1);
-  const videoRef = useRef(); 
-
-  const handlePageOneClick = () => {
-    setLevel(2);
-    videoRef.current.play();
-  }
-
-  const handleVideoEnded = () => {
-    setLevel(3);
-  }
-
   return (
-    <>
-    <PageOne 
-      level={level} 
-      handlePageOneClick={handlePageOneClick} 
-    />
-    <Intro 
-      videoRef={videoRef} 
-      handleVideoEnded={handleVideoEnded}
-      level={level} 
-    />
-    <PageTwo level={level} setLevel={setLevel} />
-    <PageThree level={level} />
-    </>
+    <Main/>
   )
 }
 
---
+++ src/components/main/Main.js
@@ -0,0 +1,39 @@
+"use client"
+
+import Intro from '@/components/intro/Intro';
+import PageOne from '@/components/pageone/PageOne';
+import PageThree from '@/components/pagethree/PageThree';
+import PageTwo from '@/components/pagetwo/PageTwo';
+import React, { useRef, useState } from 'react'
+
+const Main = () => {
+    const [level, setLevel] = useState(1);
+    const videoRef = useRef();
+
+    const handlePageOneClick = () => {
+        setLevel(2);
+        videoRef.current.play();
+    }
+
+    const handleVideoEnded = () => {
+        setLevel(3);
+    }
+
+    return (
+        <>
+            <PageOne
+                level={level}
+                handlePageOneClick={handlePageOneClick}
+            />
+            <Intro
+                videoRef={videoRef}
+                handleVideoEnded={handleVideoEnded}
+                level={level}
+            />
+            <PageTwo level={level} setLevel={setLevel} />
+            <PageThree level={level} />
+        </>
+    )
+}
+
+export default Main
